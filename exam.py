08adeadf5ebeeffbb2bb1637b8202bfc71d22274
tatuan19 <trananhtuan12a10@gmail.com>
2021-12-15 22:08:00 +0700

    Defuzzication

--- exam.py
+++ exam.py
@@ -1,9 +1,10 @@
+from scipy.integrate import quad
 from constants import *
-
 
 class QuestionNumber:
     def __init__(self, quantity):
         self.quantity = quantity
+        # Calculate C(y)
         self.fuzzyValues = {
             NUMBER_LARGE: self.__calcLargeValue(),
             NUMBER_MEDIUM: self.__calcMediumValue(),
@@ -49,55 +50,76 @@ class QuestionDifficulty:
             DIFFICULTY_VERY_EASY: distributions[3],
         }
 
+class TimeIntegral:
+    def __init__(self):
+        self.numerators = {
+            TIME_LONG: quad(self.__memberFunction, 45, 75, args=(TIME_LONG, None, NUMERATOR))[0],
+            TIME_MEDIUM: quad(self.__memberFunction, 45, 60, args=(TIME_MEDIUM, UPPER_BOUND, NUMERATOR))[0] +
+                         quad(self.__memberFunction, 30, 45, args=(TIME_MEDIUM, LOWER_BOUND, NUMERATOR))[0],
+            TIME_SHORT: quad(self.__memberFunction, 30, 40, args=(TIME_SHORT, UPPER_BOUND, NUMERATOR))[0] +
+                        quad(self.__memberFunction, 15, 30, args=(TIME_SHORT, LOWER_BOUND, NUMERATOR))[0],
+            TIME_VERY_SHORT: quad(self.__memberFunction, 5, 20, args=(TIME_VERY_SHORT, None, NUMERATOR))[0]
+        }
+        self.denominators = {
+            TIME_LONG: quad(self.__memberFunction, 45, 75, args=(TIME_LONG, None, DENOMINATOR))[0],
+            TIME_MEDIUM: quad(self.__memberFunction, 45, 60, args=(TIME_MEDIUM, UPPER_BOUND, DENOMINATOR))[0] +
+                         quad(self.__memberFunction, 30, 45, args=(TIME_MEDIUM, LOWER_BOUND, DENOMINATOR))[0],
+            TIME_SHORT: quad(self.__memberFunction, 30, 40, args=(TIME_SHORT, UPPER_BOUND, DENOMINATOR))[0] +
+                        quad(self.__memberFunction, 15, 30, args=(TIME_SHORT, LOWER_BOUND, DENOMINATOR))[0],
+            TIME_VERY_SHORT: quad(self.__memberFunction, 5, 20, args=(TIME_VERY_SHORT, None, DENOMINATOR))[0]
+        }
+        # print(self.numerators)
+        # print(self.denominators)
+
+    def __memberFunction(self, x, timeType, bound, valueType):
+        y = 0
+        if(timeType == TIME_VERY_SHORT):
+            y = 4/3 - x/15
+        if(timeType == TIME_SHORT):
+            if(bound == LOWER_BOUND):
+                y = x/15 - 1
+            if(bound == UPPER_BOUND):
+                y = 4 - x/10
+        if(timeType == TIME_MEDIUM):
+            if(bound == LOWER_BOUND):
+                y = x/15 - 2
+            if(bound == UPPER_BOUND):
+                y = 4 - x/15
+        if(timeType == TIME_LONG):
+            y = x/30 - 3/2
+        return y if valueType == DENOMINATOR else y*x
+
 
 class ExamTime:
-    def __init__(self, rules):
-        self.fuzzyValues = {
-            TIME_LONG: 0,
-            TIME_MEDIUM: 0,
-            TIME_SHORT: 0,
-            TIME_VERY_SHORT: 0
-        }
-        for rule in rules:
-            self.fuzzyValues[rule['examTimeType']] += rule['weight']
 
-    def memberFunction(self, x, ranges):
-        if(self.type == TIME_VERY_SHORT):
-            if(ranges == [5, 20]):
-                return 4/3 - x/15
-        if(self.type == TIME_SHORT):
-            if(ranges == [15, 30]):
-                return x/15 - 1
-            if(ranges == [30, 40]):
-                return 4 - x/10
-        if(self.type == TIME_MEDIUM):
-            if(ranges == [30, 45]):
-                return x/15 - 2
-            if(ranges == [45, 60]):
-                return 4 - x/15
-        if(self.type == TIME_LONG):
-            if(ranges == [45, 75]):
-                return x/30 - 3/2
-        return "Type or ranges not allowed"
-
-
-class FuzzyRules:
     def __init__(self, decisionTable, questionNumber, questionDifficulty):
-        self.rules = self.__setRules(
+        self.weights = self.__weightCalculate(
             decisionTable, questionNumber, questionDifficulty)
 
-    def __setRules(self, decisionTable, questionNumber, questionDifficulty):
-        rules = []
+    def __weightCalculate(self, decisionTable, questionNumber, questionDifficulty):
+        weightsTable = []
         for key in decisionTable:
             for element in decisionTable[key]:
                 questiondifficultyType = element[0]
                 questionNumberType = element[1]
                 weight = min(
                     questionDifficulty.fuzzyValues[questiondifficultyType], questionNumber.fuzzyValues[questionNumberType])
-                rules.append({
+                weightsTable.append({
                     'examTimeType': key,
                     'questiondifficultyType': questiondifficultyType,
                     'questionNumberType': questionNumberType,
                     'weight': weight
                 })
-        return rules
+        weights = { TIME_LONG: 0, TIME_MEDIUM: 0, TIME_SHORT: 0, TIME_VERY_SHORT: 0 }
+        for w in weightsTable:
+            weights[w['examTimeType']] += w['weight']
+
+        return weights
+
+    def defuzzy(self):
+        timeValues = TimeIntegral()
+        a = b = 0
+        for timeType, w in self.weights.items():
+            a += timeValues.numerators[timeType] * w
+            b += timeValues.denominators[timeType] * w
+        return a/b
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from constants import DECISION_TABLE, TIME_LONG
-from exam import (ExamTime, FuzzyRules, QuestionDifficulty,QuestionNumber)
+from exam import (ExamTime, QuestionDifficulty, QuestionNumber)
 
 questionNumber = QuestionNumber(50)
-questionDifficulty = QuestionDifficulty([0.2,0.3,0.3,0.2])
-fuzzyRules = FuzzyRules(DECISION_TABLE,questionNumber,questionDifficulty)
+questionDifficulty = QuestionDifficulty([0.2, 0.3, 0.3, 0.2])
+examTime = ExamTime(DECISION_TABLE, questionNumber, questionDifficulty)
 
-# print(questionNumber.fuzzyValues)
-# print(questionDifficulty.fuzzyValues)
-# for rule in fuzzyRules.rules:
-#     print(rule)
-# print(min(questionDifficulty.fuzzyValues['DIFFICULTY_HARD'], questionNumber.fuzzyValues['NUMBER_LARGE']))
+print(examTime.defuzzy())
 
-examTime = ExamTime(fuzzyRules.rules)
-print(examTime.fuzzyValues)
+
